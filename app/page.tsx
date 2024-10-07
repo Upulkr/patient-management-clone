0048987a8580a9267849bd4caf49194bad482f73
Upul <upulkr4@gmail.com>
2024-10-07 23:33:50 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-3xl text-red-400">
+      Home
+     
+    </div>
+  );
+}
+
+export default Home;
