234313444e66b981f72d412c9a5681af82c773ac
Upul <upulkr4@gmail.com>
2024-12-16 11:39:57 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 function Home() {
   return (
-    <div className="text-3xl text-red-400">
-      Home
-     
+    <div className="flex h-screen max-h-screen">
+      {/* {isAdmin && <PasskeyModal />} */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
